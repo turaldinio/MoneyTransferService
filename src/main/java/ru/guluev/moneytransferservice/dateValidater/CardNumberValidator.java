a35e213ab778fcf553bdcf83b97c9917eedabd22
Tural Guluev <tural-98@bk.ru>
2022-12-01 18:17:00 +0300

    added new exception message:The card number cannot contain letters

--- src/main/java/ru/guluev/moneytransferservice/dateValidater/CardNumberValidator.java
+++ src/main/java/ru/guluev/moneytransferservice/dateValidater/CardNumberValidator.java
@@ -9,6 +9,9 @@ import javax.validation.ConstraintValidatorContext;
 public class CardNumberValidator implements ConstraintValidator<CardNumber, String> {
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if(!s.matches("[0-9]+")){
+            throw new ErrorInputDate("The card number cannot contain letters "+s);
+        }
         if (!s.matches("\\d{16}")) {
             throw new ErrorInputDate("The card number must consist of 16 digits "+s);
         }
