d61fdb5ec357624ec168ba80e45613797b0f34ca
Tural Guluev <tural-98@bk.ru>
2022-12-04 20:24:48 +0300

    modificate the month validating

--- src/main/java/ru/guluev/moneytransferservice/validaters/CardFromValidTillValidator.java
+++ src/main/java/ru/guluev/moneytransferservice/validaters/CardFromValidTillValidator.java
@@ -1,6 +1,7 @@
 package ru.guluev.moneytransferservice.validaters;
 
 import ru.guluev.moneytransferservice.annotation.CardFromValidTill;
+import ru.guluev.moneytransferservice.exceptions.ErrorConfirmation;
 import ru.guluev.moneytransferservice.exceptions.ErrorInputDate;
 
 import javax.validation.ConstraintValidator;
@@ -17,23 +18,24 @@ public class CardFromValidTillValidator implements ConstraintValidator<CardFromV
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/yy");
         simpleDateFormat.setLenient(false);
 
-        Pattern monthAndYearPattern = Pattern.compile("\\d{2}");
-        Matcher start = monthAndYearPattern.matcher(s);
+        String[] dateArray = s.split("/");
+        if (dateArray.length != 2) {
+            throw new ErrorInputDate(String.format("The expiration date of the card is incorrectly specified (%s).Enter the date in the format **/**", s));
+        }
 
         try {
-            if (!start.lookingAt()) {
-                throw new ErrorInputDate(String.format("the month (%s) field must contain 2 digits", s));
+            if (!dateArray[0].matches("\\d{2}")) {
+                throw new ErrorInputDate(String.format("the month (%s) field must contain 2 digits [01-12]", dateArray[0]));
             }
-
-            if (!s.matches("^\\d{2}[/]{1}\\d{2}")) {
-                throw new ErrorInputDate(String.format("The expiration date of the card is incorrectly specified (%s).Enter the date in the format **/**", s));
+            if (!dateArray[1].matches("\\d{2}")) {
+                throw new ErrorInputDate(String.format("the year (%s) field must contain 2 digits", dateArray[1]));
             }
             Date date = simpleDateFormat.parse(s);
             if (date.before(new Date())) {
                 throw new ErrorInputDate(String.format("The card has expired! (%s)", s));
             }
         } catch (ParseException e) {
-            throw new ErrorInputDate("The expiration date of the card is incorrectly specified.Enter the date in the format **/**");
+            throw new ErrorInputDate(String.format("the month field (%s) should be between 01-12 ",dateArray[0]));
         }
         return true;
     }
