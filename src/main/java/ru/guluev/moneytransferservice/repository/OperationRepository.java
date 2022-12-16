8ad2c30327371e93326acbf74af0197c20978d3a
Tural Guluev <tural-98@bk.ru>
2022-12-16 17:13:10 +0300

    added OperationRepository class

---
+++ src/main/java/ru/guluev/moneytransferservice/repository/OperationRepository.java
@@ -0,0 +1,19 @@
+package ru.guluev.moneytransferservice.repository;
+
+import ru.guluev.moneytransferservice.beans.Operation;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OperationRepository {
+    private ConcurrentHashMap<AtomicInteger, Operation> map = new ConcurrentHashMap<>();
+
+    
+    public ConcurrentHashMap<AtomicInteger, Operation> getMap() {
+        return map;
+    }
+
+    public void setMap(ConcurrentHashMap<AtomicInteger, Operation> map) {
+        this.map = map;
+    }
+}
